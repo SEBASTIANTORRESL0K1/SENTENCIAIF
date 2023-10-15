552df3bf2a0e2eaf7181d1eb694e087435a4eb9c
SEBASTIANTORRESL0K1 <144284304+SEBASTIANTORRESL0K1@users.noreply.github.com>
2023-10-14 18:43:25 -0600

    New version

---
+++ main.js
@@ -0,0 +1,14 @@
+let saludo = "Hola";
+
+alert(saludo);
+
+// string = "cadena de texto";
+// number = 25;
+// booleano = true / false;
+
+let edad = prompt("¿Qué edad tienes?");
+if (edad<18) {
+	document.write("Eres menor de edad")
+}
+else
+	document.write("Eres mayor de edad")
